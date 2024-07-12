d5d803088be292fd99d69babe139e2f879a01cf7
Rachel Kim <rachelgekim@gmail.com>
2024-07-12 11:34:05 -0400

    Add footer & finish demo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 import NavBar from "@/components/NavBar";
 import Demo from "@/components/Demo";
+import Footer from "@/components/Footer";
 
 export default function Home() {
   return (
-    <>
+    <div className="h-screen w-screen">
       <NavBar />
       <Demo />
-    </>
+      <Footer />
+    </div>
   );
 }
