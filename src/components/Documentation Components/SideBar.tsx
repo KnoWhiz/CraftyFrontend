7d586c718e516b5432e223a4029dcc80f06565d8
Rachel Kim <rachelgekim@gmail.com>
2024-08-09 17:59:38 -0400

    fix sidebar in documentation

--- src/components/Documentation Components/SideBar.tsx
+++ src/components/Documentation Components/SideBar.tsx
@@ -2,7 +2,7 @@ import React, { useEffect } from "react";
 
 const SideBar = () => {
   return (
-    <aside className="font-Inter text-lg fixed left-0 top-24 w-48 flex flex-col h-full items-baseline justify-start bg-main-white text-main-black py-10 gap-y-12 pl-8 pr-2">
+    <aside className="font-Inter text-lg fixed left-0 top-24 w-48 h-[calc(100vh-6rem)] flex flex-col overflow-y-auto items-baseline justify-start bg-main-white text-main-black py-10 gap-y-12 pl-8 pr-2">
       <div className="">
         <h3 className="font-semibold">API Reference</h3>
       </div>
--- src/components/Landing Page Components/Footer.tsx
+++ src/components/Landing Page Components/Footer.tsx
@@ -1,12 +1,24 @@
+/* eslint-disable @next/next/no-img-element */
 import React from "react";
 
 const Footer = () => {
   return (
     <nav className="fixed bottom-0 flex h-20 w-full flex-row items-center justify-evenly bg-main-white pl-20 pr-16 text-main-black font-Inter border-solid border-t-[1px] border-black">
-      <p>github</p>
-      <p>twitter</p>
-      <p>linkedin</p>
-      <p>reddit</p>
+      <a href="https://github.com" target="_blank">
+        <img src="assets/logos/GitHub.svg" alt="GitHub Logo" />
+      </a>
+
+      <a href="https://twitter.com" target="_blank">
+        <img src="assets/logos/Twitter.svg" alt="Twitter Logo" />
+      </a>
+
+      <a href="https://www.linkedin.com/company/knowhiz/" target="_blank">
+        <img src="assets/logos/LinkedIn.svg" alt="LinkedIn Logo" />
+      </a>
+
+      <a href="https://reddit.com" target="_blank">
+        <img src="assets/logos/Reddit.svg" alt="Reddit Logo" />
+      </a>
     </nav>
   );
 };
